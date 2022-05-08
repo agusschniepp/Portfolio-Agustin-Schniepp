c074e916a0bc501a0d9c8cb85f11a46dced21a04
agusschniepp <agustin.schniepp@yahoo.com>
2022-05-07 23:51:03 -0300

    Portfolio Agustin Schniepp

--- js/script.js
+++ js/script.js
@@ -13,37 +13,109 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
-    var typed = new Typed(".social__icon-animation1", {
-        strings : ["ithub"], 
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
-    var typed = new Typed(".social__icon-animation2", {
-        strings : ["inkedin"], 
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
-    var typed = new Typed(".social__icon-animation3", {
-        strings : ["outube"], 
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
-    var typed = new Typed(".social__icon-animation4", {
-        strings : ["nstagram"], 
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
+    
     var typed = new Typed(".about__me-animation", {
         strings : ["Developer"], 
         typeSpeed: 100,
         backSpeed: 100,
         loop: true
     });
-
+    window.sr = ScrollReveal();
+        sr.reveal('.main-nav__list', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '-100px'
+        });
+        sr.reveal('.introduction', {
+            duration: 4000,
+            origin: 'bottom',
+            distance: '-100px'
+        });
+        sr.reveal('.social', {
+            duration: 3000,
+            origin: 'bottom',
+             distance: '-100px'
+        }); 
+        sr.reveal('.about__tittle', {
+            duration: 2000,
+            origin: 'top',
+            distance: '-100px'
+        });
+        sr.reveal('.about__img', {
+            duration: 4000,
+            origin: 'top',
+            distance: '-100px'
+        });
+        sr.reveal('.about__me', {
+            duration: 4500,
+            origin: 'top',
+            distance: '-100px'
+        }); 
+        sr.reveal('.btn-cv', {
+            duration: 5000,
+            origin: 'top',
+            distance: '-100px'
+        });
+        sr.reveal('.skills__tittle', {
+            duration: 2000,
+            origin: 'top',
+            distance: '-100px'
+        });
+        sr.reveal('.skills__html', {
+            duration: 3000,
+            origin: 'rigth',
+            distance: '-100px'
+        });
+        sr.reveal('.skills__css', {
+            duration: 3333,
+            origin: 'rigth',
+            distance: '-100px'
+        });
+        sr.reveal('.skills__javascript', {
+            duration: 3666,
+            origin: 'rigth',
+            distance: '-100px'
+        });
+        sr.reveal('.skills__photoshop', {
+            duration: 3999,
+            origin: 'rigth',
+            distance: '-100px'
+        });
+        sr.reveal('.portfolio__tittle', {
+            duration: 2000,
+            origin: 'left',
+            distance: '-100px'
+        });
+        sr.reveal('.card__tittle', {
+            duration: 2500,
+            origin: 'rigth',
+            distance: '-100px'
+        });
+        sr.reveal('.carousel', {
+            duration: 3000,
+            origin: 'top',
+            distance: '-100px'
+        });
+        sr.reveal('.contact__tittle', {
+            duration: 2000,
+            origin: 'left',
+            distance: '-100px'
+        });
+        sr.reveal('.info', {
+            duration: 2500,
+            origin: 'rigth',
+            distance: '-100px'
+        });
+        sr.reveal('.email__container', {
+            duration: 3000,
+            origin: 'top',
+            distance: '-100px'
+        });
+               
+            
+        
 });
+        
+
 
 
